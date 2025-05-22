50fec34bdad78a01fd4753e06250f69ad9625205
Yulie01 <yulietotoda01@gmail.com>
2025-05-22 09:03:27 +0200

    Updated the search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,34 +1,26 @@
-function refreshWeather ( response )
-{
-    let temperatureElement = document.querySelector( "#temperature" );
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector( "#city" );
-    
+    let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
-    
-    
 }
 
-function searchCity (city)
-{
+function searchCity(city) {
     let apiKey = "o982c70t8fd77d21494ee6b5d8a8355b";
-    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}";
-    axios.get( apiUrl ).then( refreshWeather );
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
 }
 
 
-function handleSearchSubmit (event)
-{
+function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-
-
     searchCity(searchInput.value);
 }
 
 
-let searchFormElement = document.querySelector( "#search-form" );
-searchFormElement.addEventListener("submit",handleSearchSubmit);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity( "New York" );
+searchCity("New York");
